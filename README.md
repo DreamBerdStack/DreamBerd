6c5c76490fd81d4517c7e3e3daa1b8ec9c222fb8
HaxeFloppa <123578256+HaxeFloppa@users.noreply.github.com>
2023-06-27 17:35:54 +0000

    rewrite parser, changed pretty much everything

--- README.md
+++ README.md
@@ -1,6 +1,48 @@
+<style>
+  .example1 {
+    background-color: #181818;
+    color: #055099;
+  }
+</style>
 <div align="center">
   <h1>DreamBerd</h1>
   <i>A DreamBerd parser.</i>
   <br><br>
   <a href="https://github.com/TodePond/DreamBerd">Original Concept</a>
+  <hr>
+  <h1>NOTES</h1>
 </div>
+<br>
+This parser is based off of the DreamBerd concept, written by TodePond/Lu Wilson.
+Remember, it is a concept and also that this isn't official (this is supported
+by the fact that anything with the name "DreamBerd" in it doesn't belong to TodePond's
+foundation) and therefore we've decided to change some features of the language up.
+Hope you contributors and DreamBerd devs don't mind the differences! Also, this isn't
+THE DreamBerd parser. This is just A dreamberd parser. There are people who could do
+this way better, probably even TodePond themselves considering how many languages
+they've made. Anyway, hope you enjoy this absolute mess!
+  <hr>
+  <h2>Printing</h2>
+</div>
+To print to the screen, use the <code>p</code> command (which stands for 'print') to print
+to the screen. Here's an example, which you can also read in the <code>examples/</code>
+folder:
+<br><br>
+<code class="example">
+<b># this is a comment. and yes, everything needs to end with an exclamation mark lol#!</b>
+<br>
+p("Hello, DreamBerd!")!
+</code>
+<div align="center">
+  <hr>
+  <h2>Comments</h2>
+</div>
+Defining comments is absolute hell in this, because the parser is written in python. To
+define a function, start of the line with a <code>#</code> and write whatever you need to
+write. Then write another <code>#</code> to declare the end of the comment and then the
+<code>!</code> to declare the EOL. Here is an example, taken from the Hello, DreamBerd
+example:
+<br>
+<code class="example">
+  <b># this is a comment. and yes, everything needs to end with an exclamation mark lol#!</b>
+</code>
--- parser/main.py
+++ parser/main.py
@@ -1,65 +1,84 @@
 import os
 import os.path
 
-FAST = True # run helloworld.db
+# python is fucking atrocious. - HaxeFloppa/epok_gamer/dreamberddev, Tuesday 27th June 2023, 10:52am (BST)
 
-escape_seq= {"\\\"": '"', "\\\\": "\\", "\\'": "'"}
 def main():
+    okay_stuff = ['(', ')', '!', ' ']
+    in_quotes = False
+    in_comment = False
+    return_value = 0
+    compile_lines = []
+    program_counter = 0
+    print_str = ""
     while True:
+        print("\n")
         for file in os.listdir():
             if os.path.isdir(file):
                 print(f'{file}/')
             else:
                 print(file)
-        print("===========================")
-        if not FAST:
-            open_file = input("Open a file: ")
-        else:
-            open_file = "examples/helloworld.db"
-            input()
-
-        compile_file = open(open_file, "r").read()
-        compile_lines = []
-
-        cl_add = ""
-        print_str = ""
-        print_count = 0 
-        # print(len(compile_file), compile_file) #db #the terminal is so fck laggy works fine for me
-        for letter_count in range(len(compile_file)): # if you add len you get an error. we need to remove the len command to deal with the next problem
-            if compile_file[letter_count] in  ["!"] and compile_file[letter_count -1] != "\\": # this doesn't work anymore. seems to only append one line of code, being the first line of the file.
-                compile_lines.append(cl_add)
-                cl_add = ""
-                # letter_count += 1 # this still doesn't work fuckkk
-            else:
-                cl_add += compile_file[letter_count]
-        # print(compile_lines) #db
-        compile_lines = [line.strip("\n").strip() for line in compile_lines if line]
-        # print(compile_lines) #db
-        for program_counter in range(len(compile_lines)):
-            # does this implement the print func? ye just realised this isn't even a preprocessor it's literally just a parser lol
-            # print(program_counter, compile_lines[program_counter]) #db
-            if compile_lines[program_counter].startswith("print"):
-                # starts from 0 lol FUCK xD
-                # print("print found") #db
-                if compile_lines[program_counter][5] == "(" and compile_lines[program_counter][6] == '"':
-                    # lets just iterate through the range always ... yeah 
-                    for print_count in range(7, len(compile_lines[program_counter])): # string index out of range... again
-                        if compile_lines[program_counter][print_count] == '"' and compile_lines[program_counter][print_count -1] != "\\":
-                            break
-                        if compile_lines[program_counter][print_count] != "\\" and compile_lines[program_counter][print_count -1] != "\\":
-                            print_str += compile_lines[program_counter][print_count] # HOLY SHIT IT WORKED LES FUCKING GO xDD
-                    print(print_str) 
+        print("========================")
+        open_file = input("Open a file > ")
+        compile_file = open(open_file, 'r').read()
+        for letter in compile_file:
+            compile_lines.append(letter.strip())
+        for letter in compile_lines:
+            if compile_lines[program_counter] == "p":
+                if compile_lines[program_counter - 1] == "!" or program_counter == 0 or in_comment == False:
+                    in_quotes = True
+                    program_counter += 3
+                    while not compile_lines[program_counter] == '"':
+                        print_str += compile_lines[program_counter]
+                        program_counter += 1
+                    print(print_str)
                     print_str = ""
+                    program_counter += 3
                 else:
-                    print("SyntaxError: Where have you put the brackets??? Or quotes idk lmaooo.")
-                    break
+                    continue
+            elif compile_lines[program_counter] == "#":
+                if compile_lines[program_counter - 1] == "!" or program_counter == 0 or in_comment == True:
+                    if in_comment == True:
+                        program_counter += 1
+                        in_comment = False
+                        continue
+                    else:
+                        program_counter += 1
+                        in_comment = True
+                        continue
+                else:
+                    continue
+            elif compile_lines[program_counter] == "!":
+                if in_quotes == True:
+                    continue
+                else:
+                    in_quotes = False
+                    in_comment = False
+                    program_counter += 2
+                    continue
             else:
-                if not compile_lines[program_counter].startswith("//"):
-                    print("FuncError: Bro idk what you're lookingg for.")
+                if compile_lines[program_counter] in okay_stuff:
+                    continue
+                elif compile_lines[program_counter] == '"':
+                    if in_quotes == True:
+                        in_quotes = False
+                        continue
+                    else:
+                        in_quotes = True
+                        continue
+                elif in_quotes == True:
+                    continue
+                elif compile_lines[program_counter] == '':
+                    program_counter += 1
+                    continue
+                elif in_comment == True:
+                    program_counter += 1
+                    continue
+                else:
+                    print(f'Error at line {program_counter}: {compile_lines[program_counter]} undefined.')
                     break
+        program_counter = 0
         compile_lines.clear()
-if __name__ == "__main__":
+
+if __name__ == '__main__':
     main()
-    # i have no clue what you are doing
-    # just implementing a little thing that converts a file into a list, more preferably, a dreamberd file. it seperates
-    # them by exclamation mark (will add question mark later)
